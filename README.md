d3517533f989120a30f20ca17eb8b5bd4bcb46a8
bry4n <bryann83@gmail.com>
2013-12-20 14:28:29 -0600

    Add Middleware.report for app instrument

--- README.md
+++ README.md
@@ -48,13 +48,18 @@ config.middleware.use Proceso::Middleware
 
 # config/initializers/proceso.rb
 
+Proceso::Middleware.report
+
+# or
+
 Proceso::Middleware.subscribe do |name, start, finish, id, payload|
   puts
   puts "******* PROCESO INFORMATION *******"
   mem_used = (payload[:mem_used].to_f / 1024.0).round(1)
   cpu_used = payload[:cpu_used].to_f.round(1)
+  resp_time = payload[:resp_time]
   path     = payload[:request].path_info
-  puts "MEM: #{mem_used} CPU: #{cpu_used} - PATH: #{path}"
+  puts "MEM: #{mem_used} CPU: #{cpu_used} RESP: #{resp_time} PATH: #{path}"
   puts "******* END OF PROCESO INFORMATION *******"
   puts
 end
--- lib/proceso/middleware.rb
+++ lib/proceso/middleware.rb
@@ -5,6 +5,20 @@ module Proceso
 
     SUBSCRIPTION = 'proceso.usage'
 
+    def self.report
+      subscribe do |name, start, finish, id, payload|
+        puts
+        puts "******* PROCESO INFORMATION *******"
+        mem_used    = (payload[:mem_used].to_f / 1024.0).round(1)
+        cpu_used    = payload[:cpu_used].to_f.round(1)
+        path        = payload[:request].path_info
+        resp_time   = payload[:resp_time]
+        puts "MEM: #{mem_used} CPU: #{cpu_used} RESP: #{resp_time}ms PATH: #{path} "
+        puts "******* END OF PROCESO INFORMATION *******"
+        puts
+      end
+    end
+
     def self.subscribe
       return unless defined?(ActiveSupport::Notifications)
       ActiveSupport::Notifications.subscribe(SUBSCRIPTION) do |*args|
@@ -27,22 +41,25 @@ module Proceso
     end
 
     def measure_process_usage(env)
-      mem_1 = process.mem_size
-      cpu_1 = process.user_cpu_times
+      mem_1   = process.mem_size
+      cpu_1   = process.user_cpu_times
+      resp_1  = Time.now.to_i
       response = yield
       mem_2 = process.mem_size
       cpu_2 = process.user_cpu_times
-      process_usage = process_info(env, (mem_2 - mem_1), (cpu_2 - cpu_1))
+      resp_2  = Time.now.to_i
+      process_usage = process_info(env, (mem_2 - mem_1), (cpu_2 - cpu_1), (resp_2 - resp_1))
       notifier.instrument(SUBSCRIPTION, process_usage)
       response
     end
 
-    def process_info(env, mem_used, cpu_used)
+    def process_info(env, mem_used, cpu_used, resp_time)
       req = Rack::Request.new(env)
       {
         pid: process.pid,
         mem_used: mem_used,
         cpu_used: cpu_used,
+        resp_time: resp_time,
         request: req
       }
     end
