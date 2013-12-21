7e41bba9acaedee1dd685b342671f8b338483f61
bry4n <bryann83@gmail.com>
2013-12-20 19:16:15 -0600

    Count CPU core [linux]

--- lib/proceso/linux.rb
+++ lib/proceso/linux.rb
@@ -1,2 +1,6 @@
 require_relative 'linux/proc'
 require_relative 'linux/pid'
+
+module Proceso
+  NCPU = `cat /proc/cpuinfo | egrep "core id|physical id" | tr -d "\n" | sed s/physical/\\nphysical/g | grep -v ^$ | sort | uniq | wc -l`.strip.to_i
+end
